e1e27017f3ecde669d53c39af6e1e8428288e79a
coding_xi <979675820@qq.com>
2018-01-12 22:40:49 +0800

    update

---
+++ 全局js文件/js/test01.js
@@ -0,0 +1,11 @@
+//入口函数
+$(function(){
+	var static = $("#resourceTypeId").val();
+});
+
+
+//$(document).ready(function(){
+//	alert("aaaaaaaa");
+//	$("#resourceTypeId").val("2");
+//	alert($("#resourceTypeId").val());
+//})
